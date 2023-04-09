150d3c256a093b67107702f3bcbb364a36b44165
vhumenny <v.humenny@gmail.com>
2023-04-09 20:17:39 +0300

    added exception handler

---
+++ src/main/java/com/example/online_shop/exception/exception_handling/ExceptionMessageContainer.java
@@ -0,0 +1,10 @@
+package com.example.online_shop.exception.exception_handling;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class ExceptionMessageContainer {
+    private String message;
+}
