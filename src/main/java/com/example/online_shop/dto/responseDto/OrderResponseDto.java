086610299d07c7a2fa8c22be1cb2b286d12f0819
vhumenny <v.humenny@gmail.com>
2023-04-09 19:43:55 +0300

    services in process

--- src/main/java/com/example/online_shop/dto/responseDto/OrderResponseDto.java
+++ src/main/java/com/example/online_shop/dto/responseDto/OrderResponseDto.java
@@ -1,4 +1,14 @@
 package com.example.online_shop.dto.responseDto;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Data
 public class OrderResponseDto {
+    private Long orderId;
+    private LocalDateTime orderDate;
+    private Long userId;
+    private Map<Long, Integer> products;
 }
