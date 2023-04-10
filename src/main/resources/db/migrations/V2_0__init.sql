ca4ea1df2a285b582852a5234d7c46d66dfa1a85
vhumenny <v.humenny@gmail.com>
2023-04-10 20:56:01 +0300

    added methods to ProductController and added sql v2

--- src/main/resources/db/migrations/V2_0__init.sql
+++ src/main/resources/db/migrations/V2_0__init.sql
@@ -1,6 +1,8 @@
 ALTER TABLE users
 add password varchar not null ,
-    add role varchar not null ;
+    add role varchar not null;
 
-insert into users (password,role) values ('1234','ROLE_USER') where user_id = 1;
-insert into users (password,role) values ('4321','ROLE_ADMIN') where user_id = 2;
+insert into users (password,role)
+values
+    ('1234','ROLE_USER'),
+    ('4321','ROLE_ADMIN');
