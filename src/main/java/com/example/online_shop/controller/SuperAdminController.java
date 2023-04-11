26baa8627f9a2c19b2c60f8674678f27ec16db1e
vhumenny <v.humenny@gmail.com>
2023-04-11 21:27:27 +0300

    added SuperAdminController

--- src/main/java/com/example/online_shop/controller/SuperAdminController.java
+++ src/main/java/com/example/online_shop/controller/SuperAdminController.java
@@ -12,22 +12,5 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/superadmin/admin")
 @RequiredArgsConstructor
 public class SuperAdminController {
-    private final UserService userService;
-    private final ModelMapper modelMapper;
 
-    @PostMapping
-    public UserResponseDto addAdmin(UserRequestDto userRequestDto) {
-        return userService.addUser(userRequestDto);
-    }
-
-    @DeleteMapping("/{id}")
-    public UserResponseDto deleteAdmin(@PathVariable("id") Long userId) {
-        return userService.deleteUser(userId);
-    }
-
-    @PutMapping
-    public UserResponseDto editAdmin(@RequestBody User user) {
-        UserRequestDto userRequestDto = modelMapper.map(user, UserRequestDto.class);
-        return userService.editUser(user.getUserId(), userRequestDto);
-    }
 }
