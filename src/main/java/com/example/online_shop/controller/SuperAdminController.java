ec67dd2f29aa7fde44c2c37620be84e5b35c7605
vhumenny <v.humenny@gmail.com>
2023-04-13 14:41:51 +0300

    Controllers in process

--- src/main/java/com/example/online_shop/controller/SuperAdminController.java
+++ src/main/java/com/example/online_shop/controller/SuperAdminController.java
@@ -2,10 +2,8 @@ package com.example.online_shop.controller;
 
 import com.example.online_shop.dto.requestDto.UserRequestDto;
 import com.example.online_shop.dto.responseDto.UserResponseDto;
-import com.example.online_shop.entity.User;
 import com.example.online_shop.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
